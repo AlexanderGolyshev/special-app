de85f5e3c4b5064919cf49760183c137c10879cc
Alexander Golyshev <pacangop@gmail.com>
2024-03-03 10:18:59 +0700

    test3 new2_list

--- test3.py
+++ test3.py
@@ -1,3 +1,3 @@
 my_list = list(range(1, 101))
-new1_list = list(filter(lambda x: (x % 2 == 0), my_list))
-print(new1_list)
+new2_list = list(filter(lambda x: (x % 2 == 0), my_list))
+print(new2_list)
